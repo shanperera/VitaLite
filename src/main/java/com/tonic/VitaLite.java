a86b13d687fc08dd0debd98d94761dbbcc65b29c
Shan Perera <shan.per7@gmail.com>
2026-04-12 11:42:11 +0900

    remove Vitalite java local dev env changes

--- src/main/java/com/tonic/VitaLite.java
+++ src/main/java/com/tonic/VitaLite.java
@@ -1,53 +1,82 @@
 package com.tonic;
 
+import static com.tonic.util.JVMLauncher.launchInNewJVM;
+import static com.tonic.vitalite.Versioning.getLiveRuneliteVersion;
+import static com.tonic.vitalite.Versioning.getVitaLiteVersion;
+
 import com.tonic.vitalite.Main;
 import com.tonic.vitalite.SelfUpdate;
 import com.tonic.vitalite.Versioning;
 import java.io.File;
 import java.net.URISyntaxException;
-import static com.tonic.util.JVMLauncher.launchInNewJVM;
-import static com.tonic.vitalite.Versioning.getLiveRuneliteVersion;
-import static com.tonic.vitalite.Versioning.getVitaLiteVersion;
 
 public class VitaLite {
+
     public static void main(String[] args) {
         try {
-            if(isSafeLaunch(args))
-            {
+            if (isSafeLaunch(args)) {
                 Main.main(args);
                 return;
             }
             String currentVersion = getVitaLiteVersion();
-            boolean isRunInjector = isRunInjector(args);
-            if(Versioning.isRunningFromShadedJar())
-            {
+            if (Versioning.isRunningFromShadedJar()) {
                 String liveRlVersion = getLiveRuneliteVersion();
-                if(!currentVersion.startsWith(liveRlVersion))
-                {
-                    System.out.println("VitaLite version " + currentVersion + " is out of date. Latest version is " + liveRlVersion + ".");
+                if (!currentVersion.startsWith(liveRlVersion)) {
+                    System.out.println(
+                        "VitaLite version " +
+                            currentVersion +
+                            " is out of date. Latest version is " +
+                            liveRlVersion +
+                            "."
+                    );
                     Static.getCliArgs().parse(args);
                     new SelfUpdate().checkAndUpdate();
-                    System.err.println("Warning: You are running VitaLite version " + currentVersion + " but the latest version is " + liveRlVersion + ". Please update to the latest version.");
+                    System.err.println(
+                        "Warning: You are running VitaLite version " +
+                            currentVersion +
+                            " but the latest version is " +
+                            liveRlVersion +
+                            ". Please update to the latest version."
+                    );
                     return;
                 }
-                if(!isRunInjector)
-                {
-                    String latestVitaRelease = Versioning.getLatestVitaLiteReleaseTag();
-                    if(!currentVersion.equals(latestVitaRelease))
-                    {
-                        System.out.println("VitaLite version " + currentVersion + " is out of date. Latest version is " + latestVitaRelease + ".");
-                        Static.getCliArgs().parse(args);
-                        SelfUpdate.showUpdateAvailableDialog(currentVersion, latestVitaRelease, true);
-                        System.err.println("Warning: You are running VitaLite version " + currentVersion + " but the latest version is " + latestVitaRelease + ". Please update to the latest version.");
-                        return;
-                    }
+                String latestVitaRelease =
+                    Versioning.getLatestVitaLiteReleaseTag();
+                if (!currentVersion.equals(latestVitaRelease)) {
+                    System.out.println(
+                        "VitaLite version " +
+                            currentVersion +
+                            " is out of date. Latest version is " +
+                            latestVitaRelease +
+                            "."
+                    );
+                    Static.getCliArgs().parse(args);
+                    SelfUpdate.showUpdateAvailableDialog(
+                        currentVersion,
+                        latestVitaRelease,
+                        true
+                    );
+                    System.err.println(
+                        "Warning: You are running VitaLite version " +
+                            currentVersion +
+                            " but the latest version is " +
+                            latestVitaRelease +
+                            ". Please update to the latest version."
+                    );
+                    return;
                 }
             }
-            System.out.println("VitaLite version " + currentVersion + " is up to date.");
+            System.out.println(
+                "VitaLite version " + currentVersion + " is up to date."
+            );
             String[] newArgs = new String[args.length + 1];
             newArgs[0] = "-safeLaunch";
             System.arraycopy(args, 0, newArgs, 1, args.length);
-            launchInNewJVM("com.tonic.vitalite.Main", buildFullClasspath(), newArgs);
+            launchInNewJVM(
+                "com.tonic.vitalite.Main",
+                buildFullClasspath(),
+                newArgs
+            );
             System.exit(0);
         } catch (Exception e) {
             e.printStackTrace();
@@ -56,10 +85,12 @@ public class VitaLite {
 
     private static String buildFullClasspath() throws URISyntaxException {
         String currentClasspath = System.getProperty("java.class.path");
-        File sourceLocation = new File(VitaLite.class.getProtectionDomain()
+        File sourceLocation = new File(
+            VitaLite.class.getProtectionDomain()
                 .getCodeSource()
                 .getLocation()
-                .toURI());
+                .toURI()
+        );
         String myLocation = sourceLocation.getAbsolutePath();
         if (!currentClasspath.contains(myLocation)) {
             return myLocation + File.pathSeparator + currentClasspath;
@@ -67,24 +98,9 @@ public class VitaLite {
         return currentClasspath;
     }
 
-    private static boolean isSafeLaunch(String[] args)
-    {
-        for(String arg : args)
-        {
-            if(arg.equals("-safeLaunch"))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean isRunInjector(String[] args)
-    {
-        for(String arg : args)
-        {
-            if(arg.equals("-runInjector"))
-            {
+    private static boolean isSafeLaunch(String[] args) {
+        for (String arg : args) {
+            if (arg.equals("-safeLaunch")) {
                 return true;
             }
         }
